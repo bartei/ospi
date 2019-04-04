57cf203c275cdd3e2fd71cbb77de8c6aed1b98bc
Stefano Bertelli <sbertelli@faccousa.net>
2019-04-04 17:51:28 -0500

    Removed unneded rm for pagekite configuration

--- stage-pagekite/00-pagekite/01-run.sh
+++ stage-pagekite/00-pagekite/01-run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -e
 
-# Delete the default pagekite configuration files
-rm -R					"${ROOTFS_DIR}/etc/pagekite.d/*"
-
 # Install the base_kite file used to generate the modded configurations at boot
 install -m 644 files/base_kite		"${ROOTFS_DIR}/etc/"
 
