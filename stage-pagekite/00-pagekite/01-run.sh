72dd3e1b24c2c693675f2cf71dfeb3d71bcb300c
Stefano Bertelli <sbertelli@faccousa.net>
2019-04-04 16:44:18 -0500

    Added installation of client bootstrap and bootstrap.sh

--- stage-pagekite/00-pagekite/01-run.sh
+++ stage-pagekite/00-pagekite/01-run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash -e
 
 # Delete the default pagekite configuration files
-rm -R					"${ROOTFS_DIR}/etc/pagekite/*"
+rm -R					"${ROOTFS_DIR}/etc/pagekite.d/*"
 
 # Install the base_kite file used to generate the modded configurations at boot
 install -m 644 files/base_kite		"${ROOTFS_DIR}/etc/"
+
+# Install client bootstrap service on /etc/systemd/systm
+install -m 644 files/client-bootstrap.service		"${ROOTFS_DIR}/etc/systemd/system/"
+
+# Install the bootstrap shell script into the sbin folder
+install -m 755 files/bootstrap.sh		"${ROOTFS_DIR}/sbin/"
+
+on_chroot << EOF
+systemctl enable pagekite.service
+EOF
