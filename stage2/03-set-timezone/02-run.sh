0fb3f0a232e5159d45c38ba2e57f543ce767ecca
Stefano Bertelli <sbertelli@faccousa.net>
2019-04-04 17:56:04 -0500

    Removed deletion of localtime timezone definition file.

--- stage2/03-set-timezone/02-run.sh
+++ stage2/03-set-timezone/02-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 echo "Etc/UTC" > "${ROOTFS_DIR}/etc/timezone"
-rm "${ROOTFS_DIR}/etc/localtime"
+# rm "${ROOTFS_DIR}/etc/localtime"
 
 on_chroot << EOF
 dpkg-reconfigure -f noninteractive tzdata
