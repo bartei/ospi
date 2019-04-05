4cfddc3807863bcf3fd48d10aa047f2281866ef3
Stefano Bertelli <sbertelli@faccousa.net>
2019-04-05 10:21:32 -0500

    Created interfaces.d folder in boot

--- stage1/02-net-tweaks/00-run.sh
+++ stage1/02-net-tweaks/00-run.sh
@@ -2,8 +2,11 @@
 
 install -m 644 files/ipv6.conf "${ROOTFS_DIR}/etc/modprobe.d/ipv6.conf"
 install -m 644 files/hostname "${ROOTFS_DIR}/etc/hostname"
+
+# Stefano Bertelli - This is the network configuration we intend to use, it replaces the defaults
 install -m 644 files/interfaces "${ROOTFS_DIR}/etc/network/interfaces"
 
-install -m 644 files/interfaces "${ROOTFS_DIR}/etc/network/interfaces"
+mkdir -p "${ROOTFS_DIR}/boot/interfaces.d"
+install -m 644 files/eth0 "${ROOTFS_DIR}/boot/interfaces.d"
 
 ln -sf /dev/null "${ROOTFS_DIR}/etc/systemd/network/99-default.link"
