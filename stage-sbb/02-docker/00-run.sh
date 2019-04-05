a40b11a5117ed8c9637164af1b3bd81a2253e9b1
Stefano Bertelli <sbertelli@faccousa.net>
2019-04-05 16:43:15 -0500

    Fixed typo in docker run sh

--- stage-sbb/02-docker/00-run.sh
+++ stage-sbb/02-docker/00-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-install -m 644 files/docker.list "${ROOTFS_DIR}/etc/apt/source.list.d/docker.list"
+install -m 644 files/docker.list "${ROOTFS_DIR}/etc/apt/sources.list.d/docker.list"
 
 # Install python pip for python3
 on_chroot << EOF
@@ -10,4 +10,3 @@ apt-get update
 apt-get install -y --no-install-recommends docker-ce=18.06.0~ce~3-0~raspbian
 pip install -U docker-compose
 EOF
-
