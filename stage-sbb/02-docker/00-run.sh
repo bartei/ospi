4613e289184e9eeef06e9c3ca87cb5ebfc80fd90
Stefano Bertelli <sbertelli@faccousa.net>
2019-04-05 18:03:39 -0500

    Fixed wrong package name for libssl-dev

--- stage-sbb/02-docker/00-run.sh
+++ stage-sbb/02-docker/00-run.sh
@@ -7,6 +7,6 @@ on_chroot << EOF
 wget https://download.docker.com/linux/raspbian/gpg
 apt-key add gpg
 apt-get update
-apt-get install -y --no-install-recommends docker-ce=18.06.0~ce~3-0~raspbian openssl-dev python3-dev
+apt-get install -y --no-install-recommends docker-ce=18.06.0~ce~3-0~raspbian libssl-dev python3-dev
 pip install -U docker-compose==1.23.2
 EOF
