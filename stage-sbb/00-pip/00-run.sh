d3f665c841c28a8264a02a0f54caa3bbd44e125a
Stefano Bertelli <sbertelli@faccousa.net>
2019-04-05 15:03:53 -0500

    Corrected install sequence for pip in chroot

--- stage-sbb/00-pip/00-run.sh
+++ stage-sbb/00-pip/00-run.sh
@@ -1,14 +1,12 @@
 #!/bin/bash -e
 
-# Download the pip installer from the web
-cd "${ROOTFS_DIR}/tmp/"
-wget https://bootstrap.pypa.io/get-pip.py
+# Add pip extra-index-url for the sources we intend to use
+install -m 644 files/pip.conf		"${ROOTFS_DIR}/etc/pip.conf"
 
 # Install python pip for python3
 on_chroot << EOF
 cd /tmp
+rm -f "/tmp/*.py"
+wget https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
 EOF
-
-# Add pip extra-index-url for the sources we intend to use
-install -m 644 files/pip.conf		"${ROOTFS_DIR}/etc/pip.conf"
