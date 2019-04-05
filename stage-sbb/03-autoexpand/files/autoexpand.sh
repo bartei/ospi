748d2d92d623f8bac54f6bfb3c217b41bdf95876
Stefano Bertelli <sbertelli@faccousa.net>
2019-04-05 18:45:59 -0500

    Fixed autoexpand, missing file content

---
+++ stage-sbb/03-autoexpand/files/autoexpand.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Resizing docker partition
+logger "Resizing latest partition to allow for more space for docker"
+
+parted /dev/mmcblk0 ---pretend-input-tty unit % resizepart 3 Yes 100%
+resize2fs /dev/mmcblk0p3
