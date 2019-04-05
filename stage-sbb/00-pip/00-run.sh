533c66c6b70856368c4d23eb8a137fa199fc737f
Stefano Bertelli <sbertelli@faccousa.net>
2019-04-05 14:17:05 -0500

    Removed dirt on top of run sh for stage 4

--- stage-sbb/00-pip/00-run.sh
+++ stage-sbb/00-pip/00-run.sh
@@ -1,17 +1,5 @@
 #!/bin/bash -e
 
-# Install the base_kite file used to generate the modded configurations at boot
-install -m 644 files/base_kite		"${ROOTFS_DIR}/etc/"
-
-# Install the base_kite file used to generate the modded configurations at boot
-install -m 644 files/base_hosts		"${ROOTFS_DIR}/etc/"
-
-# Install client bootstrap service on /etc/systemd/systm
-install -m 644 files/client-bootstrap.service		"${ROOTFS_DIR}/etc/systemd/system/"
-
-# Install the bootstrap shell script into the sbin folder
-install -m 755 files/bootstrap.sh		"${ROOTFS_DIR}/sbin/"
-
 # Download the pip installer from the web
 cd "${ROOTFS_DIR}/tmp/"
 wget https://bootstrap.pypa.io/get-pip.py
