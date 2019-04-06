0d4372ebd4671d22664bd10f80586fdfa9dfc2cc
Francesco Meli <fmeli@faccousa.net>
2019-04-06 12:41:43 -0500

    removed docker.list. In export-image/01-run.sh there is a apt-get -y dist-upgrade that probably upgrade docker from correct version to newer uncorrect version

--- stage-sbb/02-docker/00-run.sh
+++ stage-sbb/02-docker/00-run.sh
@@ -1,16 +1,5 @@
 #!/bin/bash -e
 
-# install -m 644 files/docker.list "${ROOTFS_DIR}/etc/apt/sources.list.d/docker.list"
-
-# Install python pip for python3
-# on_chroot << EOF
-# wget https://download.docker.com/linux/raspbian/gpg
-# apt-key add gpg
-# apt-get update
-# apt-get install -y --no-install-recommends docker-ce=18.06.0~ce~3-0~raspbian libssl-dev python3-dev
-# pip install -U docker-compose==1.23.2
-# EOF
-
 on_chroot << EOF
 export VERSION=18.06.0-ce && curl -sSL get.docker.com | sh
 EOF
