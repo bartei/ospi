920feb4cb1cb8e6064d6193ca0b195621e571371
Stefano Bertelli <sbertelli@faccousa.net>
2019-04-07 15:02:48 -0500

    Rearranged code

--- stage-sbb/02-docker/00-run.sh
+++ stage-sbb/02-docker/00-run.sh
@@ -5,8 +5,8 @@ install -m 644 files/docker-ce "${ROOTFS_DIR}/etc/apt/preferences.d/docker-ce"
 
 # Install the new fstab since we are using a separate partition for the docker containers
 log "Overwriting the original fstab with the one contemplating the var/lib/docker mount point for p4"
-
 install -m 644 files/fstab "${ROOTFS_DIR}/etc/fstab"
+
 on_chroot << EOF
 wget https://download.docker.com/linux/raspbian/gpg
 apt-key add gpg
