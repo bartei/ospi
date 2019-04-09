eafdda82167dc36ff9393776f3f7848facd11b66
Francesco Meli <fmeli@faccousa.net>
2019-04-09 12:04:48 -0500

    Removed ifplugd. Trying to use NetworkInterface

--- stage1/02-net-tweaks/00-run.sh
+++ stage1/02-net-tweaks/00-run.sh
@@ -3,19 +3,8 @@
 install -m 644 files/ipv6.conf "${ROOTFS_DIR}/etc/modprobe.d/ipv6.conf"
 install -m 644 files/hostname "${ROOTFS_DIR}/etc/hostname"
 
-# Stefano Bertelli - This is the network configuration we intend to use, it replaces the defaults
-install -m 644 files/interfaces "${ROOTFS_DIR}/etc/network/interfaces"
-
-mkdir -p "${ROOTFS_DIR}/boot/interfaces.d"
-install -m 644 files/eth0 "${ROOTFS_DIR}/boot/interfaces.d/"
-install -m 644 files/wlan0 "${ROOTFS_DIR}/boot/interfaces.d/"
+install -m 644 files/ethernet "${ROOTFS_DIR}/etc/NetworkManager/system-connections/ethernet"
+install -m 644 files/wifi "${ROOTFS_DIR}/etc/NetworkManager/system-connections/wifi"
+install -m 744 files/restart-pagekite.sh "${ROOTFS_DIR}/etc/NetworkManager/dispather.d/restart-pagekite.sh"
 
 ln -sf /dev/null "${ROOTFS_DIR}/etc/systemd/network/99-default.link"
-
-# updating ifplugd config file 
-install -m 644 files/ifplugd "${ROOTFS_DIR}/etc/default/ifplugd"
-
-# enabling ifplugd service 
-on_chroot << EOF
-systemctl enable ifplugd
-EOF
