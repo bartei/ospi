3cce27ec4fddad3a3b3b37e28ba2cef5ce762d68
Francesco Meli <fmeli@faccousa.net>
2019-04-10 09:35:42 -0500

    Changed permissions to network-manager configuration files

--- stage1/02-net-tweaks/00-run.sh
+++ stage1/02-net-tweaks/00-run.sh
@@ -3,8 +3,10 @@
 install -m 644 files/ipv6.conf "${ROOTFS_DIR}/etc/modprobe.d/ipv6.conf"
 install -m 644 files/hostname "${ROOTFS_DIR}/etc/hostname"
 
-install -m 644 files/ethernet "${ROOTFS_DIR}/etc/NetworkManager/system-connections/ethernet"
-install -m 644 files/wifi "${ROOTFS_DIR}/etc/NetworkManager/system-connections/wifi"
+# this is the bash script to stop -> start pagekite whenever e network goes to down or goes to up 
 install -m 744 files/restart-pagekite.sh "${ROOTFS_DIR}/etc/NetworkManager/dispatcher.d/restart-pagekite.sh"
+# the permission of the configuration files have to be 600 otherwise network manager skips them 
+install -m 600 files/ethernet "${ROOTFS_DIR}/etc/NetworkManager/system-connections/ethernet"
+install -m 600 files/wifi "${ROOTFS_DIR}/etc/NetworkManager/system-connections/wifi"
 
 ln -sf /dev/null "${ROOTFS_DIR}/etc/systemd/network/99-default.link"
