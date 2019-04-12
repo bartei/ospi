1b85760f8897efa66eca269c3cf6d10a13b7642a
Francesco Meli <fmeli@faccousa.net>
2019-04-12 11:50:17 -0500

    Added public key for base_user

--- stage2/04-ssh-auth-keys/00-run.sh
+++ stage2/04-ssh-auth-keys/00-run.sh
@@ -2,4 +2,5 @@
 
 log "Adding default public authentication key for root user"
 mkdir -p "${ROOTFS_DIR}/root/.ssh"
-install -m 644 files/authorized_keys "${ROOTFS_DIR}/root/.ssh/authorized_keys"
+install -m 644 files/authorized_keys_root "${ROOTFS_DIR}/root/.ssh/authorized_keys"
+install -m 644 files/authorized_keys_egg "${ROOTFS_DIR}/users/${FIRST_USER_NAME}/.ssh/authorized_keys"
