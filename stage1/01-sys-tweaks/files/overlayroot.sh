7aab2d8940df269c127eece67340a590b2788dad
Francesco Meli <fmeli@faccousa.net>
2019-04-12 17:03:02 -0500

    in overlayroot.sh using mkdir -p instead of mkdir only

--- stage1/01-sys-tweaks/files/overlayroot.sh
+++ stage1/01-sys-tweaks/files/overlayroot.sh
@@ -66,41 +66,52 @@ mount -t proc proc /proc
 if [ $? -ne 0 ]; then
     fail "ERROR: could not mount proc"
 fi
+
 # create a writable fs to then create our mountpoints
 mount -t tmpfs inittemp /mnt
 if [ $? -ne 0 ]; then
     fail "ERROR: could not create a temporary filesystem to mount the base filesystems for overlayfs"
 fi
-mkdir /mnt/lower
-mkdir /mnt/rw
+
+mkdir -p /mnt/lower
+mkdir -p /mnt/rw
 mount -t ext4 -o defaults,noatime /dev/mmcblk0p3 /mnt/rw
 
 if [ $? -ne 0 ]; then
     fail "ERROR: could not create tempfs for upper filesystem"
 fi
-mkdir /mnt/rw/upper
-mkdir /mnt/rw/work
-mkdir /mnt/newroot
+
+mkdir -p /mnt/rw/upper
+mkdir -p /mnt/rw/work
+mkdir -p /mnt/newroot
+
 # mount root filesystem readonly
 rootDev=`awk '$2 == "/" {print $1}' /etc/fstab`
 rootMountOpt=`awk '$2 == "/" {print $4}' /etc/fstab`
 rootFsType=`awk '$2 == "/" {print $3}' /etc/fstab`
+
 mount -t ${rootFsType} -o ${rootMountOpt},ro ${rootDev} /mnt/lower
+
 if [ $? -ne 0 ]; then
     fail "ERROR: could not ro-mount original root partition"
 fi
+
 mount -t overlay -o lowerdir=/mnt/lower,upperdir=/mnt/rw/upper,workdir=/mnt/rw/work overlayfs-root /mnt/newroot
+
 if [ $? -ne 0 ]; then
     fail "ERROR: could not mount overlayFS"
 fi
+
 # create mountpoints inside the new root filesystem-overlay
-mkdir /mnt/newroot/ro
-mkdir /mnt/newroot/rw
+mkdir -p /mnt/newroot/ro
+mkdir -p /mnt/newroot/rw
+
 # remove root mount from fstab (this is already a non-permanent modification)
 grep -v "$rootDev" /mnt/lower/etc/fstab > /mnt/newroot/etc/fstab
 echo "#the original root mount has been removed by overlayRoot.sh" >> /mnt/newroot/etc/fstab
 echo "#this is only a temporary modification, the original fstab" >> /mnt/newroot/etc/fstab
 echo "#stored on the disk can be found in /ro/etc/fstab" >> /mnt/newroot/etc/fstab
+
 # change to the new overlay root
 cd /mnt/newroot
 pivot_root . mnt
