f56ea7a1ea8c6b25a597f23344e5f2de9b835689
Francesco Meli <fmeli@faccousa.net>
2019-04-12 14:41:54 -0500

    renamed stage-pagekite to stage-base and added ipython as a pip package instead of apt package

---
+++ stage-base/00-pip/00-run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+# Add pip extra-index-url for the sources we intend to use
+install -m 644 files/pip.conf		"${ROOTFS_DIR}/etc/pip.conf"
+
+# Install python pip for python3
+on_chroot << EOF
+cd /tmp
+rm -f "/tmp/*.py"
+wget https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
+EOF
---
+++ stage-base/01-ipython/00-run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+# Install ipython
+on_chroot << EOF
+pip install ipython
+EOF
