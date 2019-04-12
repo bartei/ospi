493bce85cdd68543b2f4ff440ddac5b5a579e0e3
Francesco Meli <fmeli@faccousa.net>
2019-04-12 12:01:48 -0500

    Typo last commit

--- stage2/04-ssh-auth-keys/00-run.sh
+++ stage2/04-ssh-auth-keys/00-run.sh
@@ -3,4 +3,4 @@
 log "Adding default public authentication key for root user"
 mkdir -p "${ROOTFS_DIR}/root/.ssh"
 install -m 644 files/authorized_keys_root "${ROOTFS_DIR}/root/.ssh/authorized_keys"
-install -m 644 files/authorized_keys_egg "${ROOTFS_DIR}/users/${FIRST_USER_NAME}/.ssh/authorized_keys"
+install -m 644 files/authorized_keys_base_user "${ROOTFS_DIR}/users/${FIRST_USER_NAME}/.ssh/authorized_keys"
