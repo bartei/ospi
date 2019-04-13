f7e584cf8c2e3d837431cdbbf3aaa667688be569
pinkynrg <biggyapple@gmail.com>
2019-04-13 14:14:38 -0500

    Replaced client-services crontab with systemd service

--- stage-sbb/02-client-services/00-run.sh
+++ stage-sbb/02-client-services/00-run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -e
 
-install -m 644 files/client-services-cron "${ROOTFS_DIR}/etc/cron.d/"
-
 # Install python pip for python3
 on_chroot << EOF
 pip install -U client-services-master
 EOF
 
+# add client-services file
+install -m 644 files/client-services.service "${ROOTFS_DIR}/etc/systemd/system/client-services.service"
+
+# finally enable service
+on_chroot << EOF
+systemctl enable client-services
+EOF
