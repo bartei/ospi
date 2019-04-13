bec366daf7b9bfa9677619f4d48192873616b99e
pinkynrg <biggyapple@gmail.com>
2019-04-13 15:43:56 -0500

    Removed fstab from stage1. Keeping everying in stage-base

--- stage1/01-sys-tweaks/00-run.sh
+++ stage1/01-sys-tweaks/00-run.sh
@@ -2,7 +2,6 @@
 
 install -d "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d"
 install -m 644 files/noclear.conf "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/noclear.conf"
-install -v -m 644 files/fstab "${ROOTFS_DIR}/etc/fstab"
 install -m 755 files/overlayroot.sh "${ROOTFS_DIR}/sbin/overlayroot.sh"
 
 on_chroot << EOF
