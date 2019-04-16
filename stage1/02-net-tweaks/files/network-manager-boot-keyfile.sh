924016596ce1decbbb6ea1f5237737fea35baeea
Francesco Meli <fmeli@faccousa.net>
2019-04-16 18:01:11 -0500

    Added dos2unix package and now converting fiels from boot to NetworkManager converting using it

--- stage1/02-net-tweaks/files/network-manager-boot-keyfile.sh
+++ stage1/02-net-tweaks/files/network-manager-boot-keyfile.sh
@@ -3,5 +3,7 @@
 # Install network configuration files for NetworkManager
 for CONFIG_FILE in `ls /boot/keyfiles`
 do
+# converts files from dos format to unix 
+dos2unix "/boot/keyfiles/${CONFIG_FILE}"
 install -m 600 "/boot/keyfiles/${CONFIG_FILE}" "/etc/NetworkManager/system-connections/${CONFIG_FILE}"
 done
