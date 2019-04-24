b3c72ae8b156f829cfe503767fb8714498a8227b
Francesco Meli <fmeli@faccousa.net>
2019-04-24 18:03:19 -0500

    Moved custom sys sweaks from stage 2 to stage base - part 2

--- stage-base/_03-sys-tweaks/00-run.sh
+++ stage-base/_03-sys-tweaks/00-run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -e
 
+rm -rf "${ROOTFS_DIR}/etc/init.d/resize2fs_once"
+install -m 755 files/rc.local "${ROOTFS_DIR}/etc/rc.local"
 install -m 755 files/overlayroot.sh "${ROOTFS_DIR}/sbin/overlayroot.sh"
+
+on_chroot <<EOF
+apt-get autoremove --purge dphys-swapfile
+EOF
