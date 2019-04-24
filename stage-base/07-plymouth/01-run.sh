aa4b6f301e1802afadaf1c3617bd1c09e57e890b
Stefano Bertelli <stefano@provvedo.com>
2019-04-23 23:07:43 -0500

    Fixed wrong paths in run.sh

--- stage-base/07-plymouth/01-run.sh
+++ stage-base/07-plymouth/01-run.sh
@@ -7,9 +7,9 @@ log "Create the theme directory"
 mkdir -p "${ROOTFS_DIR}/usr/share/plymouth/themes/faccos"
 
 log "Install the theme files into the new directory"
-install -m 644 files/facco-splash.png "${ROOTFS_DIR}/usr/share/plymouth/themes/facco-splash.png"
-install -m 644 files/faccos.plymouth "${ROOTFS_DIR}/usr/share/plymouth/themes/faccos.plymouth"
-install -m 644 files/faccos.script "${ROOTFS_DIR}/usr/share/plymouth/themes/faccos.script"
+install -m 644 files/facco-splash.png "${ROOTFS_DIR}/usr/share/plymouth/themes/faccos/facco-splash.png"
+install -m 644 files/faccos.plymouth "${ROOTFS_DIR}/usr/share/plymouth/themes/faccos/faccos.plymouth"
+install -m 644 files/faccos.script "${ROOTFS_DIR}/usr/share/plymouth/themes/faccos/faccos.script"
 
 log "Now configure plymouthd to use the newly created boot theme"
 install -m 644 files/plymouthd.conf "${ROOTFS_DIR}/etc/plymouth/plymouthd.conf"
