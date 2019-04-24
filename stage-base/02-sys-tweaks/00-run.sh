4c4d9a74bfa9661e6839308863fdcc0d4f061ee5
Francesco Meli <fmeli@faccousa.net>
2019-04-24 18:51:03 -0500

    additional sys sweaks for showing os info on ssh login and inital prompt with general info (ip, hostname, kernel, ...)

--- stage-base/02-sys-tweaks/00-run.sh
+++ stage-base/02-sys-tweaks/00-run.sh
@@ -1,9 +1,22 @@
 #!/bin/bash -e
 
-rm -rf "${ROOTFS_DIR}/etc/init.d/resize2fs_once"
-install -m 755 files/rc.local "${ROOTFS_DIR}/etc/rc.local"
-install -m 755 files/overlayroot.sh "${ROOTFS_DIR}/sbin/overlayroot.sh"
-
+# remove unused system stock packages
 on_chroot <<EOF
 apt-get autoremove --purge dphys-swapfile
 EOF
+
+# provide a more detailed login prompt
+install -m 644 files/issue "${ROOTFS_DIR}/etc/issue"
+
+# clear screen after boot from a getty 
+install -m 644 files/getty@.service "${ROOTFS_DIR}/systemd/system/getty@.service"
+
+# give message of the current version of the OS when connecting with SSH
+install -m 644 files/motd "${ROOTFS_DIR}/etc/motd"
+install -m 755 files/20-issue "${ROOTFS_DIR}/etc/update-motd.d/20-issue"
+
+# remove stock auto resize 
+rm -rf "${ROOTFS_DIR}/etc/init.d/resize2fs_once"
+
+# add shell script for mounting overlay fs
+install -m 755 files/overlayroot.sh "${ROOTFS_DIR}/sbin/overlayroot.sh"
