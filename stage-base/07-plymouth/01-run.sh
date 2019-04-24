5cb7a3e1c01be0cf0cd4749b284701c819755ee6
Stefano Bertelli <stefano@provvedo.com>
2019-04-23 22:21:17 -0500

    Replaced comments with logs in the newly created run.sh for plymouth

--- stage-base/07-plymouth/01-run.sh
+++ stage-base/07-plymouth/01-run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 
-# Install the modified cmdline.txt which disables the raspberry logos and uses the splash in quiet mode
+log "Install the modified cmdline.txt which disables the raspberry logos and uses the splash in quiet mode"
 install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/cmdline.txt"
 
-# Create the theme directory
+log "Create the theme directory"
 mkdir -p "${ROOTFS_DIR}/usr/share/plymouth/themes/faccos"
 
-# Install the theme files into the new directory
+log "Install the theme files into the new directory"
 install -m 644 files/facco-splash.png "${ROOTFS_DIR}/usr/share/plymouth/themes/facco-splash.png"
 install -m 644 files/faccos.plymouth "${ROOTFS_DIR}/usr/share/plymouth/themes/faccos.plymouth"
 install -m 644 files/faccos.script "${ROOTFS_DIR}/usr/share/plymouth/themes/faccos.script"
 
-# Now configure plymouthd to use the newly created boot theme
+log "Now configure plymouthd to use the newly created boot theme"
 install -m 644 files/plymouthd.conf "${ROOTFS_DIR}/etc/plymouth/plymouthd.conf"
