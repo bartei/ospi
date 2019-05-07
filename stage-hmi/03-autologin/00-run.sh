e8521227772511e702a4fe12fd292a12ebd396f8
Francesco Meli <fmeli@faccousa.net>
2019-05-07 18:09:54 -0500

    Added base_user autologin for HMI

---
+++ stage-hmi/03-autologin/00-run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+# clear screen after boot from a getty 
+install -m 644 files/autologin.conf "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/autologin.conf"
+perl -pi -e "s/PLACEHOLDER_BASE_USER/${$FIRST_USER_NAME}/g" "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/autologin.conf"
+
+# remove unused system stock packages
+on_chroot <<EOF
+ln -fs /lib/systemd/system/getty@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
+EOF
