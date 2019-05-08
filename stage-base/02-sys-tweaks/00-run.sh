141be05626039fa6a7bcfc2a1b5ac17579e67f0b
pinkynrg <biggyapple@gmail.com>
2019-05-07 19:36:01 -0500

    Corrected destination for getty@.service 2

--- stage-base/02-sys-tweaks/00-run.sh
+++ stage-base/02-sys-tweaks/00-run.sh
@@ -9,7 +9,7 @@ EOF
 install -m 644 files/issue "${ROOTFS_DIR}/etc/issue"
 
 # clear screen after boot from a getty 
-install -m 644 files/getty@.service "${ROOTFS_DIR}/var/systemd/system/getty@.service"
+install -m 644 files/getty@.service "${ROOTFS_DIR}/lib/systemd/system/getty@.service"
 
 # give message of the current version of the OS when connecting with SSH
 install -m 644 files/motd "${ROOTFS_DIR}/etc/motd"
