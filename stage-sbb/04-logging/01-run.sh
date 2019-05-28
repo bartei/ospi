fb50813d2b7df3a70258fc5302295232945345bf
Francesco Meli <fmeli@faccousa.net>
2019-05-28 13:43:50 -0500

    Added forgotten log rotate file for client-web image

--- stage-sbb/04-logging/01-run.sh
+++ stage-sbb/04-logging/01-run.sh
@@ -1,5 +1,8 @@
 # copy rsyslog configuration file for client-web
-install -m 600 files/00-client-web.conf "${ROOTFS_DIR}/etc/rsyslog.d/00-client-web.conf"
+install -m 600 files/rsyslog.conf "${ROOTFS_DIR}/etc/rsyslog.d/00-client-web.conf"
+
+# copy log rotate configuration file
+install -m 644 files/logrotate "${ROOTFS_DIR}/etc/logrotate.d/client-web"
 
 # execute logrotate hourly 
 on_chroot << EOF
