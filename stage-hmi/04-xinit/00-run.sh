2f2642197fa7c6af42fcff9de46c1457056c899a
Stefano Bertelli <sbertelli@faccousa.net>
2019-07-15 14:17:51 -0500

    Putting chown in chroot as it doesn't work when in normal env.

--- stage-hmi/04-xinit/00-run.sh
+++ stage-hmi/04-xinit/00-run.sh
@@ -3,4 +3,9 @@
 install -m 644 files/.xinitrc "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.xinitrc"
 install -m 644 files/xinit.sh "${ROOTFS_DIR}/etc/profile.d/xinit.sh"
 
+# Change the owner of .xinitrc to allow changes performed by the egg user on the startup of the system
+# Eg. when you want your home page to load a different address from the stock one
+on_chroot <<EOF
 chown ${FIRST_USER_NAME}:${FIRST_USER_NAME} "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.xinitrc"
+EOF
+
