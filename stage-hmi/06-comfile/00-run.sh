e3e0502c3cbd5192d071398d7b45626b2df54c89
Stefano Bertelli <sbertelli@faccousa.net>
2019-07-15 10:15:11 -0500

    Added pigpio package to control the gpio lines of the hmi Modified the pigpiod.service startup script for the CM
    Cleaned up config.txt file and prepared section for the display
    resolution configuration according to the device used.

--- stage-hmi/06-comfile/00-run.sh
+++ stage-hmi/06-comfile/00-run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
-# clear screen after boot from a getty 
 # Install the modified version of rc.local which turns on the backlight on the Comfile pi 15"
 install -m 644 files/rc.local "${ROOTFS_DIR}/etc/"
+
+# Install the modified version of the pigpiod startup script to be compatible with the CM
+install -m 644 files/pigpiod.service "${ROOTFS_DIR}/lib/systemd/system/"
