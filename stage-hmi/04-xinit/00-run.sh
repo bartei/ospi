c26492be3e8b18e9655524f88abaee7292d5c3c3
Stefano Bertelli <sbertelli@faccousa.net>
2019-07-15 10:20:58 -0500

    Changed ownership of .xinitrc for the system unprivileged user.

--- stage-hmi/04-xinit/00-run.sh
+++ stage-hmi/04-xinit/00-run.sh
@@ -2,3 +2,5 @@
 
 install -m 644 files/.xinitrc "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.xinitrc"
 install -m 644 files/xinit.sh "${ROOTFS_DIR}/etc/profile.d/xinit.sh"
+
+chown ${FIRST_USER_NAME}:${FIRST_USER_NAME} "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.xinitrc"
