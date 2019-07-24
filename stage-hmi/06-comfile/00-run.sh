e329a885dfe1a91c8c899814b46ecb6f3116644b
Stefano Bertelli <sbertelli@faccousa.net>
2019-07-24 09:58:57 -0500

    Added some logging to the new scripts

--- stage-hmi/06-comfile/00-run.sh
+++ stage-hmi/06-comfile/00-run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
 # Install the modified version of rc.local which turns on the backlight on the Comfile pi 15"
+log "Installing new rc.local for backlight"
 install -m 644 files/rc.local "${ROOTFS_DIR}/etc/"
 
 # Install the modified version of the pigpiod startup script to be compatible with the CM
+log "Modifying the current pigpiod service for the Comfile devices"
 install -m 644 files/pigpiod.service "${ROOTFS_DIR}/lib/systemd/system/"
