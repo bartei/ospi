fa8ec384f3bffef03252acd85dd40fe27ec44285
Stefano Bertelli <sbertelli@faccousa.net>
2019-07-31 09:18:23 -0500

    Reverted overlayroot for non-hmi images and created new step in the hmi for sys-tweaks.

--- stage-hmi/08-sys-tweaks/files/overlayroot.sh
+++ stage-hmi/08-sys-tweaks/files/overlayroot.sh
@@ -45,87 +45,37 @@
 #  To install software, run upgrades and do other changes to the raspberry setup, simply remove the init=
 #  entry from the cmdline.txt file and reboot, make the changes, add the init= entry and reboot once more.
 
-fail(){
-        echo -e "$1"
-        /bin/bash
-}
-
-# load module
-modprobe overlay
-if [ $? -ne 0 ]; then
-    fail "ERROR: missing overlay kernel module"
-fi
-
-modprobe btrfs
-if [ $? -ne 0 ]; then
-    fail "ERROR: missing btrfs kernel module"
-fi
-
-# mount /proc
-mount -t proc proc /proc
-if [ $? -ne 0 ]; then
-    fail "ERROR: could not mount proc"
-fi
-
-# create a writable fs to then create our mountpoints
-mount -t tmpfs inittemp /mnt
-if [ $? -ne 0 ]; then
-    fail "ERROR: could not create a temporary filesystem to mount the base filesystems for overlayfs"
-fi
-
-mkdir -p /mnt/lower
-mkdir -p /mnt/rw
-mount -t ext4 -o defaults,noatime /dev/mmcblk0p3 /mnt/rw
-
-if [ $? -ne 0 ]; then
-    fail "ERROR: could not create tempfs for upper filesystem"
-fi
-
-mkdir -p /mnt/rw/upper
-mkdir -p /mnt/rw/work
-mkdir -p /mnt/newroot
-
-# mount root filesystem readonly
-rootDev=`awk '$2 == "/" {print $1}' /etc/fstab`
-rootMountOpt=`awk '$2 == "/" {print $4}' /etc/fstab`
-rootFsType=`awk '$2 == "/" {print $3}' /etc/fstab`
-
-mount -t ${rootFsType} -o ${rootMountOpt},ro ${rootDev} /mnt/lower
-
-if [ $? -ne 0 ]; then
-    fail "ERROR: could not ro-mount original root partition"
-fi
-
-mount -t overlay -o lowerdir=/mnt/lower,upperdir=/mnt/rw/upper,workdir=/mnt/rw/work overlayfs-root /mnt/newroot
-
-if [ $? -ne 0 ]; then
-    fail "ERROR: could not mount overlayFS"
-fi
-
-# create mountpoints inside the new root filesystem-overlay
-mkdir -p /mnt/newroot/ro
-mkdir -p /mnt/newroot/rw
-
-# remove root mount from fstab (this is already a non-permanent modification)
-grep -v "$rootDev" /mnt/lower/etc/fstab > /mnt/newroot/etc/fstab
-echo "#the original root mount has been removed by overlayRoot.sh" >> /mnt/newroot/etc/fstab
-echo "#this is only a temporary modification, the original fstab" >> /mnt/newroot/etc/fstab
-echo "#stored on the disk can be found in /ro/etc/fstab" >> /mnt/newroot/etc/fstab
+python3 /sbin/overlayroot.py
 
 # change to the new overlay root
 cd /mnt/newroot
 pivot_root . mnt
+
 exec chroot . sh -c "$(cat <<END
 # move ro and rw mounts to the new root
-mount --move /mnt/mnt/lower/ /ro
+
+echo "Moving /mnt/mnt/lower"
+mount --move /mnt/mnt/lower /ro
 if [ $? -ne 0 ]; then
     echo "ERROR: could not move ro-root into newroot"
-    /bin/bash
 fi
+
+echo "Moving /mnt/mnt/rw"
 mount --move /mnt/mnt/rw /rw
 if [ $? -ne 0 ]; then
-    echo "ERROR: could not move tempfs rw mount into newroot"
-    /bin/bash
+    echo "ERROR: could not move rw mount into newroot"
+fi
+
+echo "Moving /mnt/mnt/persist"
+mount --move /mnt/mnt/persist /persist
+if [ $? -ne 0 ]; then
+    echo "ERROR: could not move persist mount into newroot"
+fi
+
+echo "Moving /mnt/mnt/squash"
+mount --move /mnt/mnt/squash /squash
+if [ $? -ne 0 ]; then
+    echo "ERROR: count not move squash mount into newroot"
 fi
 
 # unmount unneeded mounts so we can unmout the old readonly root
