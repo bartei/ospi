1d2a949856a129ee1f5e064d116499bf61b03e1e
pinkynrg <biggyapple@gmail.com>
2019-08-07 10:00:11 +0200

    Removed some packages and service fixes that werent needed

--- stage-hmi/06-comfile/00-run.sh
+++ stage-hmi/06-comfile/00-run.sh
@@ -1,13 +1,5 @@
 #!/bin/bash -e
 
-# Install the modified version of rc.local which turns on the backlight on the Comfile pi 15"
-log "Installing new rc.local for backlight"
-install -m 644 files/rc.local "${ROOTFS_DIR}/etc/"
-
-# Install the modified version of the pigpiod startup script to be compatible with the CM
-log "Modifying the current pigpiod service for the Comfile devices"
-install -m 644 files/pigpiod.service "${ROOTFS_DIR}/lib/systemd/system/"
-
 # service to turn on backlight on 15 inches comfiles
 log "Added service to turn backlight on for 15 inches comfiles"
 install -m 744 files/backlight.sh "${ROOTFS_DIR}/sbin/backlight.sh"
