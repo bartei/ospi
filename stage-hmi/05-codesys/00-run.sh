158f864c3ffc4545956dc5c8d9ae1e1d6701ca73
pinkynrg <biggyapple@gmail.com>
2019-08-30 15:11:54 +0200

    Updated version of codesys from V3.5.14.30 to V3.5.14.40

--- stage-hmi/05-codesys/00-run.sh
+++ stage-hmi/05-codesys/00-run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-install -m 644 files/codesyscontrol_arm_raspberry_V3.5.14.30.deb "${ROOTFS_DIR}/root/codesyscontrol_arm_raspberry_V3.5.14.30.deb"
+install -m 644 files/codesyscontrol_arm_raspberry_V3.5.14.40.deb "${ROOTFS_DIR}/root/codesyscontrol_arm_raspberry_V3.5.14.40.deb"
 
 on_chroot <<EOF
-dpkg -i "/root/codesyscontrol_arm_raspberry_V3.5.14.30.deb"
+dpkg -i "/root/codesyscontrol_arm_raspberry_V3.5.14.40.deb"
 systemctl enable codesyscontrol
 EOF
