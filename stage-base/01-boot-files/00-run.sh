a79e2ea3e54485194a9f312f981ffe162346786b
Stefano Bertelli <stefano.bertelli@b2z-insurance.com>
2024-02-14 22:54:42 -0600

    fix: update bookworm release from pigen

--- stage-base/01-boot-files/00-run.sh
+++ stage-base/01-boot-files/00-run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -e
 
-install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/cmdline.txt"
-install -m 644 files/config.txt "${ROOTFS_DIR}/boot/config.txt"
+install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/firmware/"
+if ! [ -L "${ROOTFS_DIR}/boot/cmdline.txt" ]; then
+	ln -s firmware/cmdline.txt "${ROOTFS_DIR}/boot/cmdline.txt"
+fi
+
+install -m 644 files/config.txt "${ROOTFS_DIR}/boot/firmware/"
+if ! [ -L "${ROOTFS_DIR}/boot/config.txt" ]; then
+	ln -s firmware/config.txt "${ROOTFS_DIR}/boot/config.txt"
+fi
