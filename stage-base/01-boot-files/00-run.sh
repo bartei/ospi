455ec585f6c188d781b870fcebebe5b4e51ceb7e
Stefano Bertelli <stefano@provvedo.com>
2024-02-16 18:32:12 -0600

    fix: boot files for stage base

--- stage-base/01-boot-files/00-run.sh
+++ stage-base/01-boot-files/00-run.sh
@@ -1,11 +1,4 @@
 #!/bin/bash -e
 
 install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/firmware/"
-if ! [ -L "${ROOTFS_DIR}/boot/cmdline.txt" ]; then
-	ln -s firmware/cmdline.txt "${ROOTFS_DIR}/boot/cmdline.txt"
-fi
-
 install -m 644 files/config.txt "${ROOTFS_DIR}/boot/firmware/"
-if ! [ -L "${ROOTFS_DIR}/boot/config.txt" ]; then
-	ln -s firmware/config.txt "${ROOTFS_DIR}/boot/config.txt"
-fi
