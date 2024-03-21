a1034534909a17133d8887c9f7ac26cac6d44602
Stefano Bertelli <stefano@provvedo.com>
2024-03-20 19:49:50 -0500

    fix: add step to install rotary-controller-python in the image

---
+++ stage-base/40-rotary-controller/01-run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+on_chroot <<EOF
+cd /root
+git clone https://github.com/bartei/rotary-controller-python.git
+cd rotary-controller-python
+poetry run pip install kivy[base]
+poetry install
+EOF
+
+
+on_chroot <<EOF
+cd /root
+cd rotary-controller-python
+bash install-service.sh
+EOF
