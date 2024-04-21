0b0741da36ef24abced440b990ad6dac43024a80
Stefano Bertelli <stefano@provvedo.com>
2024-04-21 12:18:31 -0500

    fix: legacy mode for installing kivy deps

--- stage-base/17-rotary-controller/01-run.sh
+++ stage-base/17-rotary-controller/01-run.sh
@@ -4,7 +4,6 @@ on_chroot <<EOF
 cd /root
 git clone https://github.com/bartei/rotary-controller-python.git
 cd rotary-controller-python
-export KIVY_DEPS_ROOT=/kivy-deps-build/kivy-dependencies
 poetry run pip install kivy[base]
 poetry install
 EOF
