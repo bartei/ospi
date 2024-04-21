e0570bfb3e2810f66cd116e6a94c39858728bbd8
Stefano Bertelli <stefano@provvedo.com>
2024-04-21 11:39:24 -0500

    fix: update kivy build

--- stage-base/17-rotary-controller/01-run.sh
+++ stage-base/17-rotary-controller/01-run.sh
@@ -4,6 +4,7 @@ on_chroot <<EOF
 cd /root
 git clone https://github.com/bartei/rotary-controller-python.git
 cd rotary-controller-python
+export KIVY_DEPS_ROOT=/kivy-deps-build/kivy-dependencies
 poetry run pip install kivy[base]
 poetry install
 EOF
