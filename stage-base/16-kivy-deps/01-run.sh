957ef12ef3c8d5652a2065847b4c9c9941798c37
Stefano Bertelli <stefano@provvedo.com>
2024-04-21 12:18:21 -0500

    fix: legacy mode for installing kivy deps

--- stage-base/16-kivy-deps/01-run.sh
+++ stage-base/16-kivy-deps/01-run.sh
@@ -1,62 +1,63 @@
 #!/bin/bash -e
 
+# Install libpng:
 on_chroot <<EOF
-mkdir /kivy-deps-build
-cd /kivy-deps-build
-wget https://raw.githubusercontent.com/kivy/kivy/master/tools/build_linux_dependencies.sh
-chmod +x build_linux_dependencies.sh
-./build_linux_dependencies.sh
-export KIVY_DEPS_ROOT=/kivy-deps-build/kivy-dependencies
-python -m pip install "kivy[base]" kivy_examples --no-binary kivy
+wget https://downloads.sourceforge.net/project/libpng/libpng16/1.6.40/libpng-1.6.40.tar.gz
+tar -zxvf libpng-1.6.40.tar.gz
+pushd libpng-1.6.40
+./configure
+make -j$(nproc)
+make install
+popd
 EOF
 
-# # Install SDL2:
-# on_chroot <<EOF
-# wget https://libsdl.org/release/SDL2-2.0.10.tar.gz
-# tar -zxvf SDL2-2.0.10.tar.gz
-# pushd SDL2-2.0.10
-# ./configure --enable-video-kmsdrm --disable-video-opengl --disable-video-x11 --disable-video-rpi
-# make -j$(nproc)
-# make install
-# popd
-# EOF
-#
-#
-# # Install SDL2_image:
-# on_chroot <<EOF
-# wget https://libsdl.org/projects/SDL_image/release/SDL2_image-2.0.5.tar.gz
-# tar -zxvf SDL2_image-2.0.5.tar.gz
-# pushd SDL2_image-2.0.5
-# ./configure
-# make -j$(nproc)
-# make install
-# popd
-# EOF
-#
-# # Install SDL2_mixer:
-# on_chroot <<EOF
-# wget https://libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.4.tar.gz
-# tar -zxvf SDL2_mixer-2.0.4.tar.gz
-# pushd SDL2_mixer-2.0.4
-# ./configure
-# make -j$(nproc)
-# make install
-# popd
-# EOF
-#
-# # Install SDL2_ttf:
-# on_chroot <<EOF
-# wget https://libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.15.tar.gz
-# tar -zxvf SDL2_ttf-2.0.15.tar.gz
-# pushd SDL2_ttf-2.0.15
-# ./configure
-# make -j$(nproc)
-# make install
-# popd
-# EOF
-#
-# # Make sure the dynamic libraries cache is updated:
-# on_chroot <<EOF
-# ldconfig -v
-# adduser "$USER" render
-# EOF
+# Install SDL2:
+on_chroot <<EOF
+wget https://libsdl.org/release/SDL2-2.28.5.tar.gz
+tar -zxvf SDL2-2.28.5.tar.gz
+pushd SDL2-2.28.5
+./configure --enable-video-kmsdrm --disable-video-opengl --disable-video-x11 --disable-video-rpi
+make -j$(nproc)
+make install
+popd
+EOF
+
+# Install SDL2_image:
+on_chroot <<EOF
+wget https://libsdl.org/projects/SDL_image/release/SDL2_image-2.8.0.tar.gz
+tar -zxvf SDL2_image-2.8.0.tar.gz
+pushd SDL2_image-2.8.0
+./configure
+make -j$(nproc)
+make install
+popd
+EOF
+
+# Install SDL2_mixer:
+on_chroot <<EOF
+wget https://libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.6.3.tar.gz
+tar -zxvf SDL2_mixer-2.6.3.tar.gz
+pushd SDL2_mixer-2.6.3
+./configure
+make -j$(nproc)
+make install
+popd
+EOF
+
+# Install SDL2_ttf:
+on_chroot <<EOF
+wget https://libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.20.2.tar.gz
+tar -zxvf SDL2_ttf-2.20.2.tar.gz
+pushd SDL2_ttf-2.20.2
+./configure
+make -j$(nproc)
+make install
+popd
+EOF
+
+
+# Make sure the dynamic libraries cache is updated:
+on_chroot <<EOF
+ldconfig -v
+adduser "$USER" render
+EOF
