bf92e5b2e4f0477ca042adceb0d0fc318a4a9827
Stefano Bertelli <stefano.bertelli@b2z-insurance.com>
2024-05-12 01:17:05 -0500

    fix: revert bookworm

--- stage-base/15-poetry/01-run.sh
+++ stage-base/15-poetry/01-run.sh
@@ -2,11 +2,6 @@
 
 # finally enable services 
 on_chroot <<EOF
-export POETRY_NO_INTERACTION=1
-export POETRY_VIRTUALENVS_CREATE=false
-export POETRY_CACHE_DIR='/var/cache/pypoetry'
-export POETRY_HOME='/usr/local'
-export POETRY_VERSION=1.8.2
-
-curl -sSL https://install.python-poetry.org | python3 -
+echo "nothing to do"
+# pip install poetry --break-system-packages
 EOF
