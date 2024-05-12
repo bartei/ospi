5fa4be5578784901d68a4c55849bb81d607d62ac
Stefano Bertelli <stefano.bertelli@b2z-insurance.com>
2024-05-12 00:00:00 -0500

    fix: update rotary controller installation

--- stage-base/15-poetry/01-run.sh
+++ stage-base/15-poetry/01-run.sh
@@ -2,6 +2,11 @@
 
 # finally enable services 
 on_chroot <<EOF
-echo "nothing to do"
-# pip install poetry --break-system-packages
+export POETRY_NO_INTERACTION=1
+export POETRY_VIRTUALENVS_CREATE=false
+export POETRY_CACHE_DIR='/var/cache/pypoetry'
+export POETRY_HOME='/usr/local'
+export POETRY_VERSION=1.8.2
+
+curl -sSL https://install.python-poetry.org | python3 -
 EOF
--- stage-base/17-rotary-controller/01-run.sh
+++ stage-base/17-rotary-controller/01-run.sh
@@ -4,6 +4,15 @@ on_chroot <<EOF
 cd /root
 git clone https://github.com/bartei/rotary-controller-python.git
 cd rotary-controller-python
+git fetch -p
+git checkout dev
+
+export POETRY_NO_INTERACTION=1 \
+export POETRY_VIRTUALENVS_CREATE=false \
+export POETRY_CACHE_DIR='/var/cache/pypoetry' \
+export POETRY_HOME='/usr/local' \
+export POETRY_VERSION=1.8.2
+
 poetry run pip install kivy[base]
 poetry install
 EOF
