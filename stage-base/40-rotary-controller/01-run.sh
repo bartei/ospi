8fb04056eaf74e4be0aebb1d229a3002bdec9909
Stefano Bertelli <stefano@provvedo.com>
2024-05-16 18:12:21 -0500

    fix: test again if we can get a bookworm image built

---
+++ stage-base/40-rotary-controller/01-run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+on_chroot <<EOF
+cd /root
+git clone https://github.com/bartei/rotary-controller-python.git
+cd rotary-controller-python
+pip install .
+cp systemd/rotary-controller.service /etc/systemd/system
+EOF
