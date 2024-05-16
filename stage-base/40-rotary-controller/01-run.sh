e9053798ab70876df5db8cd7b7cfed2611570660
Stefano Bertelli <stefano@provvedo.com>
2024-05-16 18:42:30 -0500

    fix: allow break system packages for pip

--- stage-base/40-rotary-controller/01-run.sh
+++ stage-base/40-rotary-controller/01-run.sh
@@ -4,6 +4,6 @@ on_chroot <<EOF
 cd /root
 git clone https://github.com/bartei/rotary-controller-python.git
 cd rotary-controller-python
-pip install .
+pip install . --break-system-packages
 cp systemd/rotary-controller.service /etc/systemd/system
 EOF
