9554b3b76039d83aeaee7bbe0bdcd5c9f64049e1
Stefano Bertelli <stefano@provvedo.com>
2024-05-27 14:10:35 -0500

    fix: sync with pigen arm64, it's for stages not used

---
+++ stage-base/10-lightdm/00-run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+install -m 755 files/resize2fs_once	"${ROOTFS_DIR}/etc/init.d/"
+
+# remove unused system stock packages
+on_chroot <<EOF
+apt-get autoremove --purge -yy dphys-swapfile
+systemctl enable resize2fs_once
+EOF
+
+# provide a more detailed login prompt
+install -m 644 files/issue "${ROOTFS_DIR}/etc/issue"
+
+# clear screen after boot from a getty 
+install -m 644 files/getty@.service "${ROOTFS_DIR}/lib/systemd/system/getty@.service"
+
+# give message of the current version of the OS when connecting with SSH
+install -m 644 files/motd "${ROOTFS_DIR}/etc/motd"
+install -m 755 files/20-issue "${ROOTFS_DIR}/etc/update-motd.d/20-issue"
