d276ddc2304119a475ea77c29cece3fb3df3e3ed
Stefano Bertelli <stefano.bertelli@b2z-insurance.com>
2024-05-26 22:27:12 -0500

    fix: tweaks to the boot sequence

--- stage-base/40-rotary-controller/01-run.sh
+++ stage-base/40-rotary-controller/01-run.sh
@@ -6,4 +6,5 @@ git clone https://github.com/bartei/rotary-controller-python.git
 cd rotary-controller-python
 pip install . --break-system-packages
 cp systemd/rotary-controller.service /etc/systemd/system
+systemctl enable rotary-controller.service
 EOF
