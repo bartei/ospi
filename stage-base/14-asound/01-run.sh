a0d08eb8643d7c83d3ad6f9e563b0e75225bc7be
Stefano Bertelli <stefano@provvedo.com>
2024-09-08 16:32:54 -0500

    fix: typo in asound

--- stage-base/14-asound/01-run.sh
+++ stage-base/14-asound/01-run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
 
 log "Configure default audio device as the HDMI display"
-install -m 644 files/splash.png "${ROOTFS_DIR}/usr/share/plymouth/themes/ospi/splash.png"
-
+install -m 644 files/asound.conf "${ROOTFS_DIR}/etc/asound.conf"
