2794b7cd2a4bdd447c9366ceb67e493d4faab539
Stefano Bertelli <stefano@provvedo.com>
2024-12-14 18:30:18 -0600

    major: wip for new raspberry release compatible with pi5

---
+++ stage-base/40-rotary-controller/01-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+on_chroot <<EOF
+cd /root
+git clone https://github.com/bartei/rotary-controller-python.git
+cd rotary-controller-python
+python3 -m venv ./venv
+source ./venv/bin/activate
+pip install .
+cp systemd/rotary-controller.service /etc/systemd/system
+systemctl daemon-reload || true
+systemctl enable rotary-controller.service || true
+EOF
