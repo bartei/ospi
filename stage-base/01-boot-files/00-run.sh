29303ee59ba17b3709c1837bb1eb39c341ad5552
Stefano Bertelli <stefano@provvedo.com>
2024-12-16 11:51:33 -0600

    fix: configure asound with the proper new card id

--- stage-base/01-boot-files/00-run.sh
+++ stage-base/01-boot-files/00-run.sh
@@ -3,3 +3,4 @@
 install -m 644 files/config.txt "${ROOTFS_DIR}/boot/firmware/"
 
 sed -i "s/console=serial0,115200//" "${ROOTFS_DIR}/boot/firmware/cmdline.txt"
+echo " dwc_otg.lpm_enable=0" >> "${ROOTFS_DIR}/boot/firmware/cmdline.txt"
