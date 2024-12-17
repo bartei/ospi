65d887e56bdf87908743551881cfb4e2f9b3f52b
Stefano Bertelli <stefano@provvedo.com>
2024-12-16 20:58:46 -0600

    fix: source for uv path resolution

--- stage-base/40-rotary-controller/01-run.sh
+++ stage-base/40-rotary-controller/01-run.sh
@@ -5,6 +5,7 @@ cd /root
 git clone https://github.com/bartei/rotary-controller-python.git
 cd rotary-controller-python
 curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
 uv venv
 uv sync
 cp systemd/rotary-controller.service /etc/systemd/system
