515ffd2eb54f2bacd119ffeffb9634ab4b400b52
Stefano Bertelli <stefano@provvedo.com>
2024-12-16 22:13:23 -0600

    fix: revert to pip for packages installation

--- stage-base/40-rotary-controller/01-run.sh
+++ stage-base/40-rotary-controller/01-run.sh
@@ -4,10 +4,9 @@ on_chroot <<EOF
 cd /root
 git clone https://github.com/bartei/rotary-controller-python.git
 cd rotary-controller-python
-curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
-uv venv
-uv sync
+python3 -m venv ./venv
+source ./venv/bin/activate
+pip install .
 cp systemd/rotary-controller.service /etc/systemd/system
 systemctl daemon-reload || true
 systemctl enable rotary-controller.service || true
