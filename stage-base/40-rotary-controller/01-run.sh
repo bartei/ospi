eb26529b3edd46892e93b46ff0a9cdc494eba829
Stefano Bertelli <stefano@provvedo.com>
2024-12-16 19:06:53 -0600

    fix: use uv to install rcp

--- stage-base/40-rotary-controller/01-run.sh
+++ stage-base/40-rotary-controller/01-run.sh
@@ -4,9 +4,9 @@ on_chroot <<EOF
 cd /root
 git clone https://github.com/bartei/rotary-controller-python.git
 cd rotary-controller-python
-python3 -m venv ./venv
-source ./venv/bin/activate
-pip install .
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv venv
+uv sync
 cp systemd/rotary-controller.service /etc/systemd/system
 systemctl daemon-reload || true
 systemctl enable rotary-controller.service || true
