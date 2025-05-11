8ebec41ad3fbae582059191e3fa04c1972794fbb
Stefano Bertelli <stefano@provvedo.com>
2025-05-11 15:02:33 -0500

    fix: update rcp version

--- stage-base/40-rotary-controller/01-run.sh
+++ stage-base/40-rotary-controller/01-run.sh
@@ -4,7 +4,7 @@ install -v -m 644 files/rcp.service "${ROOTFS_DIR}/etc/systemd/system/rcp.servic
 install -v -m 755 files/start.sh "${ROOTFS_DIR}/start.sh"
 
 on_chroot <<EOF
-git clone --depth 1 --branch "v1.0.31" https://github.com/bartei/rotary-controller-python.git
+git clone --depth 1 --branch "v1.0.32" https://github.com/bartei/rotary-controller-python.git
 cd rotary-controller-python
 pip install .
 systemctl enable rcp.service || true
