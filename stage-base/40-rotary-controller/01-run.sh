6c4bb43c2a7a98abf50160c1f29c0b72d55065c9
Stefano Bertelli <stefano@provvedo.com>
2025-06-08 14:48:39 -0500

    fix: remove installation of uv

--- stage-base/40-rotary-controller/01-run.sh
+++ stage-base/40-rotary-controller/01-run.sh
@@ -4,7 +4,6 @@ install -v -m 644 files/rcp.service "${ROOTFS_DIR}/etc/systemd/system/rcp.servic
 install -v -m 755 files/start.sh "${ROOTFS_DIR}/start.sh"
 
 on_chroot <<EOF
-wget -qO- https://astral.sh/uv/install.sh | sh
 cd /
 rm -rf /rotary-controller-python
 git clone --depth 1 --branch "v1.0.32" https://github.com/bartei/rotary-controller-python.git
