bf454880c42e253a043d4e65d8b44b3415b6f482
Stefano Bertelli <stefano@provvedo.com>
2025-06-08 14:47:12 -0500

    fix: cmdline and config for rcp fix: enable build for bookworm fix: startup service for rcp and entry point

--- stage-base/40-rotary-controller/01-run.sh
+++ stage-base/40-rotary-controller/01-run.sh
@@ -6,10 +6,14 @@ install -v -m 755 files/start.sh "${ROOTFS_DIR}/start.sh"
 on_chroot <<EOF
 wget -qO- https://astral.sh/uv/install.sh | sh
 cd /
+rm -rf /rotary-controller-python
 git clone --depth 1 --branch "v1.0.32" https://github.com/bartei/rotary-controller-python.git
 cd rotary-controller-python
-uv venv
-uv sync
+
+virtualenv --clear venv
+source ./venv/bin/activate
+pip install .
+
 systemctl enable rcp.service || true
 systemctl enable NetworkManager || true
 EOF
--- stage-base/40-rotary-controller/files/start.sh
+++ stage-base/40-rotary-controller/files/start.sh
@@ -2,4 +2,5 @@
 export KCFG_KIVY_KEYBOARD_MODE="systemanddock"
 export KCFG_KIVY_LOG_DIR="/var/log"
 
-uv run python /rotary-controller-python/rcp/main.py
+source /rotary-controller-python/venv/bin/activate
+python /rotary-controller-python/rcp/main.py
