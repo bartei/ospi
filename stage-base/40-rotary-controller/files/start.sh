1392b3f5785e1f264a510c04b710428bd4f38d51
Stefano Bertelli <stefano@provvedo.com>
2025-06-08 16:41:27 -0500

    fix: silly mistake for env activation

--- stage-base/40-rotary-controller/files/start.sh
+++ stage-base/40-rotary-controller/files/start.sh
@@ -2,5 +2,5 @@
 export KCFG_KIVY_KEYBOARD_MODE="systemanddock"
 export KCFG_KIVY_LOG_DIR="/var/log"
 
-source /rotary-controller-python/venv/bin/activate
+. /rotary-controller-python/venv/bin/activate
 python /rotary-controller-python/rcp/main.py
